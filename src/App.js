eeb130ea9672b6fa185b7084ae1db24e542f7ee1
Nil Andreu <nilandreug@gmail.com>
2021-05-02 20:15:11 +0200

    Cleaning the initial files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-/**For creating this redux app, I have  npx create-react-app disney-plus-clone --template redux*/
+/**For creating this redux app, I have  npx create-react-app disney-plus-clone --template redux
+ * Redux what allows us is to save the state of a component
+*/
 import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
@@ -7,51 +9,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      
     </div>
   );
 }

e56ccf4afcc0aa4a88e2b95ff1f8c315a8121c6f
Nil Andreu <nilandreug@gmail.com>
2021-05-02 21:36:56 +0200

    Images added

---
+++ src/components/Header.js
@@ -0,0 +1,41 @@
+/**We are going to use the styled components */
+import styled from 'styled-components'
+
+import React from 'react'
+
+/**Here we define the general component (header in this case) */
+function Header() {
+    return (
+        <div>
+            <Nav>
+                <Logo src="./public/images/logo.svg" />
+                <NavMenu>
+
+                </NavMenu>
+            </Nav>
+        </div>
+    )
+}
+
+/**We define each part of the navbar */
+const Nav = styled.nav`
+    height: 70px;
+    background: #090b13;
+`;
+
+const Logo = styled.img`
+    width: 80px;
+`;
+
+const NavMenu = styled.ul`
+    
+`;
+
+
+export default Header;
+
+/**When we select a word and put Command+D, gonna select all the same words in the file.
+ * This is useful when changing a name to a variable
+ */
+
+
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Home() {
+    return (
+        <div>
+            Home
+        </div>
+    )
+}
+
+/**Gonna export this component to be able to import it in a different file */
+export default Home;

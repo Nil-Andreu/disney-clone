afe64b8de4d9312fcf9ad3ea9b79e84547dccc46
Nil Andreu <nilandreug@gmail.com>
2021-05-03 12:28:42 +0200

    add viewers

--- src/components/Viewers.jsx
+++ src/components/Viewers.jsx
@@ -8,16 +8,16 @@ function Viewers() {
                 <img src="/images/viewers-disney.png" alt=""/>
             </Wrap>
             <Wrap>
-                <img src="/images/viewers-disney.png" alt=""/>
+                <img src="/images/viewers-pixar.png" alt=""/>
             </Wrap>
             <Wrap>
-                <img src="/images/viewers-disney.png" alt=""/>
+                <img src="/images/viewers-marvel.png" alt=""/>
             </Wrap>
             <Wrap>
-                <img src="/images/viewers-disney.png" alt=""/>
+                <img src="/images/viewers-starwars.png" alt=""/>
             </Wrap>
             <Wrap>
-                <img src="/images/viewers-disney.png" alt=""/>
+                <img src="/images/viewers-national.png" alt=""/>
             </Wrap>
         </Container>
     )
@@ -37,9 +37,12 @@ const Container = styled.div`
 `;
 
 const Wrap = styled.div`
+    cursor: pointer;
+
     box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
-        rgb(0 0 0 / 73%) 0 16px 10px -10px;    rgb
+        rgb(0 0 0 / 73%) 0 16px 10px -10px;
     border-radius: 10px;
+
     // Gonna add very light border
     border: 3px solid rgba(249, 249, 249, 0.1);
 
@@ -50,6 +53,18 @@ const Wrap = styled.div`
 
         // Going to say the object to cover as much as possible
         object-fit: cover;
+    }
+
+    // We transition everything (all)
+    // The 250ms is the perfect number, not too slow or fast. That's why is used by a lot of developers
+    transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+
+    //Remember the & concatenates the hover with the wrap
+    &:hover {
+        box-shadow: rgba(0 0 0 / 80%) 0px 40px 58px -16px,
+            rgb(0 0 0 / 72%) 0px 30px 22px -10px;
+        transform: scale(1.05);
+        border-color: rgba(249, 249, 249, 0.8);
     }
 `;
 

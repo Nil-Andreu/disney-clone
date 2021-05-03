f3575d40d67a76313f2785d8c69755752329a2b3
Nil Andreu <nilandreug@gmail.com>
2021-05-03 15:07:33 +0200

    Simpson image

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -3,10 +3,47 @@ import styled from 'styled-components';
 
 function Movies() {
     return (
-        <div>
-            
-        </div>
+        <Container>
+            <Heading>Recommended for You</Heading>
+            <Content>
+                <Wrap>
+                    <img src="images/disney-simpson.jpeg" />
+                </Wrap>
+                <Wrap>
+                    <img src="" />
+                </Wrap>
+                <Wrap>
+                    <img src="" />
+                </Wrap>
+                <Wrap>
+                    <img src="" />
+                </Wrap>
+                <Wrap>
+                    <img src="" />
+                </Wrap>
+            </Content>
+        </Container>
     )
 }
+
+const Container = styled.div`
+    
+`;
+
+const Heading = styled.h4``;
+
+const Content = styled.div`
+    display: grid;
+    grid-gap: 
+    grid-template-columns: repeat(4, minmax(0, 1fr));
+`;
+
+const Wrap = styled.div`
+
+    img {
+        width: 400px;
+        height: auto;
+    }
+`;
 
 export default Movies

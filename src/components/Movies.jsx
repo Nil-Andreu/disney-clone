203196c547dd4eb4d7ff6b8e3eb8a0db5dd1c409
Nil Andreu <nilandreug@gmail.com>
2021-05-04 09:01:25 +0200

    Movies done

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -7,19 +7,19 @@ function Movies() {
             <Heading>Recommended for You</Heading>
             <Content>
                 <Wrap>
-                    <img src="images/disney-simpson.jpeg" />
+                    <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg" alt=""/>
                 </Wrap>
                 <Wrap>
-                    <img src="" />
+                    <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg" alt=""/>
                 </Wrap>
                 <Wrap>
-                    <img src="" />
+                    <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg" alt=""/>
                 </Wrap>
                 <Wrap>
-                    <img src="" />
+                    <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg" alt=""/>
                 </Wrap>
                 <Wrap>
-                    <img src="" />
+                    <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg" alt=""/>
                 </Wrap>
             </Content>
         </Container>
@@ -34,15 +34,33 @@ const Heading = styled.h4``;
 
 const Content = styled.div`
     display: grid;
-    grid-gap: 
+    grid-gap: 25px;
     grid-template-columns: repeat(4, minmax(0, 1fr));
 `;
 
 const Wrap = styled.div`
+    border-radius: 10px;
+    cursor: pointer;
+    overflow: hidden;
+    border: 3px solid rgba(249, 249, 249, 0.1);
+    box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+        rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+    transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+
 
     img {
-        width: 400px;
-        height: auto;
+        width: 100%;
+        height: 100%;
+
+        // To cover the image within the div
+        object-fit: cover;
+    }
+
+    &:hover {
+        transform: scale(1.05);
+        border-color: rgba(249, 249, 249, 0.8);
+        box-shadow: rgb(0 0 0 / 80%) 0px 40px 38px -16px,
+            rba(0 0 0 / 72%) 0px 30px 22px -10px;
     }
 `;
 

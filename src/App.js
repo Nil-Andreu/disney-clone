7d6d90b266b748998c16d1d221defecdc05d7714
Nil Andreu <nilandreug@gmail.com>
2021-05-04 09:09:21 +0200

    React routes for the rooutes

--- src/App.js
+++ src/App.js
@@ -10,12 +10,33 @@ import Header from './components/Header.jsx';
 /**For the home page */
 import Home from './components/Home.jsx';
 
+// And for the detail page
+import Detail from './components/Detail.jsx';
+
+/**Need to install react-router-dom to switch between pages */
+import {BrowserRouter as Router, 
+        Switch,
+        Route,
+        Link 
+      } from "react-router-dom"
+
+/**WE define the routes of out page. By default, all of them are going to have the header
+* Then i have the switch, where i gonna have different components depending on the route
+*/
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home>
-      </Home>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="detail">
+            <Detail />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>      
     </div>
   );
 }

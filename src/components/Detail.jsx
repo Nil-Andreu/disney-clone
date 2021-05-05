c92dab3be059ee3f2863481f6015093b35db89e6
Nil Andreu <nilandreug@gmail.com>
2021-05-05 21:14:52 +0200

    trailer and play button

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -11,6 +11,25 @@ function Detail() {
             <ImgTitle>
                 <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/D7AEE1F05D10FC37C873176AAA26F777FC1B71E7A6563F36C6B1B497CAB1CEC2/scale?width=1440&aspectRatio=1.78" alt=""/>
             </ImgTitle>
+
+            <Controls>
+                <PlayButton>
+                    <img src="/images/play-icon-black.png" alt=""/>
+                    <span>PLAY</span>
+                </PlayButton>
+                <TrailerButton>
+                    <img src="/images/play-icon-white.png" alt=""/>
+                    <span>TRAILER</span>
+                </TrailerButton>
+                <AddButton>
+                    <img src="/images/play-icon-black.png" alt=""/>
+                    <span>PLAY</span>
+                </AddButton>
+                <GroupWatchButton>
+                    <img src="/images/play-icon-black.png" alt=""/>
+                    <span>PLAY</span>
+                </GroupWatchButton>
+            </Controls>
         </Container>
     )
 }
@@ -56,5 +75,50 @@ const ImgTitle = styled.div`
         object-fit: contain;
     }
 `;
+
+const Controls = styled.div`
+    display: flex;
+    flex-direction: row;
+`;
+
+const PlayButton = styled.button`
+    border-radius: 4px;
+    margin-right: 22px;
+    font-size: 15px;
+    display: flex;
+    align-items: center;
+    height: 56px;
+    background-color: rgb(240, 249, 249);
+    border: none;
+    padding: 0 24px;
+    letter-spacing: 1.8px;
+    cursor: pointer;
+
+    &:hover {
+        background: rba(198, 198, 198);
+    }
+`;
+
+const TrailerButton = styled.button`
+    border-radius: 4px;
+    margin-right: 22px;
+    font-size: 15px;
+    display: flex;
+    align-items: center;
+    height: 56px;
+    background-color: rgb(240, 249, 249);
+    border: none;
+    padding: 0 24px;
+    letter-spacing: 1.8px;
+    cursor: pointer;
+
+    &:hover {
+        background: rba(198, 198, 198);
+    }
+`;
+
+const AddButton = styled.button``;
+
+const GroupWatchButton = styled.button``;
 
 export default Detail

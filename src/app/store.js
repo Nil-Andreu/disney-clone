25813f7f6206fca533ca55c06a9e40e268edf1a0
Nil Andreu <nilandreug@gmail.com>
2021-05-12 17:34:45 +0200

    Store.js done

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import movieReducer from '../features/movie/movieSlice';
+import userReducer from '../features/user/userSlice.jsx'
 
 export const store = configureStore({
   reducer: {
     movie: movieReducer, //We tell the app we have this reducer at our disposal
+    user: userReducer,
   },
 });

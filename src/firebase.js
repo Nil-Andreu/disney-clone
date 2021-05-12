58f97c4c0152bbf0e59607dc2fd928083dcdff19
Nil Andreu <nilandreug@gmail.com>
2021-05-12 17:33:21 +0200

    User slice

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 /**For using the firebase, we install firebase and firebase-tools with npm.
  * In this file we are going to have the configuration for the fireabse
-*/
+ */
 
 import firebase from "firebase"; //First we input the firebase
 
